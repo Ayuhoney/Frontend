1d044a238d787e016d0418dd9fef08318edea882
ayush <ayush2211207@gmail.com>
2024-01-03 23:51:55 +0530

    Add Product Controller

---
+++ server/controller/product-controller.js
@@ -0,0 +1,15 @@
+
+import Product from '../model/productSchema.js'
+
+export const getProducts = async(req,res)=>{
+
+    try {
+
+        const products = await Product.find({})
+        res.status(200).json(products);
+        
+    } catch (error) {
+        
+        res.status(500).json({message : error.message});
+    }
+}
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,10 +1,14 @@
 import express from  'express';
 const router = express.Router();
 
+
 import { userSignUp, userLogIn } from '../controller/user-controller.js';
+import {getProducts} from '../controller/product-controller.js'
 
 //login & signup
 router.post('/signup', userSignUp);
 router.post('/login', userLogIn);
+
+router.get('/products',getProducts);
 
 export default router;
