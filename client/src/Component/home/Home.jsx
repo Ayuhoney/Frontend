9c846e3a66acc81664a19aa9d2751fa064c059de
ayush <ayush2211207@gmail.com>
2024-01-04 02:18:08 +0530

    Apply Redux

--- client/src/Component/home/Home.jsx
+++ client/src/Component/home/Home.jsx
@@ -1,7 +1,11 @@
 import { Banner } from "./Banner"
 import { NavBar } from "./NavBar"
 import { Box,styled } from "@mui/material"
-import { LoginDialog } from "../login/LoginDialog";
+
+import {getProducts} from '../../redux/actions/productActions'
+import { useEffect } from "react";
+import { useDispatch ,useSelector } from "react-redux";
+
 
 
 const Component = styled(Box)`
@@ -9,14 +13,25 @@ const Component = styled(Box)`
     padding:0px 7px 15px 7px;
     background:#F2F2F2;
 `;
+
 export const Home = () => {
+
+     const {products} = useSelector(state =>state.getProducts); //redux store state hai naki jo import kie h
+     console.log(products);
+     
+     const dispatch = useDispatch();
+
+      useEffect(()=>{
+
+        dispatch(getProducts());
+
+      },[dispatch]);
   return (
     <>
       <NavBar />
       <Component>
         <Banner />
       </Component>
-      <LoginDialog/>
     </>
   )
 }
